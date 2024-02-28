8ecad49774760b046e8f47ccf32cb72d03923c3e
joshtin2505 <justincastro2505@gmail.com>
2024-02-28 17:45:32 -0500

    Add app.ts file with routes and middlewares

---
+++ src/app.ts
@@ -0,0 +1,34 @@
+import express from 'express'
+import cors from 'cors'
+import morgan from 'morgan'
+import usersRoutes from './routes/users.routes'
+const app = express()
+
+// Middlewares
+app.use(cors())
+app.use(morgan('dev'))
+app.use(express.json())
+
+// Routes
+// -------
+app.get('/', (_req, res) => {
+  res.json({
+    message: 'ROOT',
+    entpoints: {
+      root: '/'
+    }
+  })
+})
+app.use('/users', usersRoutes)
+
+app.use(function (_req, res, _next) {
+  res.status(404).json({
+    message: 'Not Found',
+    entpoints: {
+      root: '/'
+    }
+  })
+})
+
+export const PORT = 3000
+export default app
