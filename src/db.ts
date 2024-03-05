91b86935d406d26219979344bab5a227209b2b12
joshtin2505 <justincastro2505@gmail.com>
2024-03-05 16:40:22 -0500

    Add function for db connect

--- src/db.ts
+++ src/db.ts
@@ -14,4 +14,17 @@ const pool = new Pool({
   password: '5t2eOI4chqjCQ2quTUHmvFmSN89AGT3J',
   port: 5432
 })
+
+export function dbConect(PORT: number) {
+  pool
+    .connect()
+    .then((client) => {
+      console.log('connected to database')
+      console.log(`Server is running on port http://localhost:${PORT} 🚀`)
+      client.release()
+    })
+    .catch((err) => {
+      console.log('error connecting to database', err)
+    })
+}
 export default pool
