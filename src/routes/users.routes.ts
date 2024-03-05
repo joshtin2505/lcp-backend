950a7c966903ec5eb3a67129ca20c7245c3c65a7
joshtin2505 <justincastro2505@gmail.com>
2024-03-04 20:07:24 -0500

    Update user routes and add authentication middleware

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -2,13 +2,20 @@ import { Router } from 'express'
 import {
   getAllUsers,
   getUserById,
-  usersRoutes
+  usersRoutes,
+  addUser,
+  updateUser,
+  deleteUser
 } from '../controllers/users.controllers'
+import { authMasterAdmin } from '../middlewares/validateRoll.middlewares'
 
 const router = Router()
 
 router.get('/', usersRoutes)
-router.get('/all', getAllUsers)
-router.get('/:userId', getUserById)
+router.get('/all', authMasterAdmin, getAllUsers)
+router.get('/:userId', authMasterAdmin, getUserById)
+router.post('/add', authMasterAdmin, addUser)
+router.put('/update', authMasterAdmin, updateUser)
+router.delete('/delete/:userId', authMasterAdmin, deleteUser)
 
 export default router
