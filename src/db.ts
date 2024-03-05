0cf1f52535f3d0662e2d4fc3ec59bdd71ec54aa2
joshtin2505 <justincastro2505@gmail.com>
2024-03-05 15:31:12 -0500

    Actualizar configuración de la base de datos

--- src/db.ts
+++ src/db.ts
@@ -1,10 +1,17 @@
 import { Pool } from 'pg'
 
+// const pool = new Pool({
+//   user: 'postgres',
+//   host: 'localhost',
+//   database: 'casitadepapel',
+//   password: 'password',
+//   port: 5432
+// })
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
+  user: 'casitadepapel',
+  host: 'dpg-cnjntc21hbls73dqroi0-a',
   database: 'casitadepapel',
-  password: 'password',
+  password: '5t2eOI4chqjCQ2quTUHmvFmSN89AGT3J',
   port: 5432
 })
 export default pool
