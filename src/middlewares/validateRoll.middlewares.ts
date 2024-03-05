eb3a3ca50d0803c4315ccabdd4d0f73535b23d31
joshtin2505 <justincastro2505@gmail.com>
2024-03-04 20:07:18 -0500

    Refactor authentication middleware to use destructuring

--- src/middlewares/validateRoll.middlewares.ts
+++ src/middlewares/validateRoll.middlewares.ts
@@ -1,40 +1,36 @@
 import type { Request, Response, NextFunction } from 'express'
+
 interface RequestWithUser extends Request {
   user: {
-    roll: string
+    roll: string | undefined
   }
 }
 
 export const authMasterAdmin = (
-  req: RequestWithUser,
+  req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  if (req.user.roll !== 'masterAdmin') {
+  const { user } = req as RequestWithUser
+  if (user.roll !== 'masterAdmin') {
     return res.status(403).json({ message: 'Forbidden' })
   }
   next()
   return res.status(204).json({ message: 'Auth' }) // This line is not necessary but it's a good practice to have it
 }
 
-export const authAdmin = (
-  req: RequestWithUser,
-  res: Response,
-  next: NextFunction
-) => {
-  if (req.user.roll !== 'admin') {
+export const authAdmin = (req: Request, res: Response, next: NextFunction) => {
+  const { user } = req as RequestWithUser
+  if (user.roll !== 'admin') {
     return res.status(403).json({ message: 'Forbidden' })
   }
   next()
   return res.status(204).json({ message: 'Auth' }) // This line is not necessary but it's a good practice to have it
 }
 
-export const authUser = (
-  req: RequestWithUser,
-  res: Response,
-  next: NextFunction
-) => {
-  if (req.user.roll !== 'user') {
+export const authUser = (req: Request, res: Response, next: NextFunction) => {
+  const { user } = req as RequestWithUser
+  if (user.roll !== 'user') {
     return res.status(403).json({ message: 'Forbidden' })
   }
   next()
