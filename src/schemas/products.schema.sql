ce591c871eb97a78aad65417d5e4a19b6114e07e
Joshtin <91860458+joshtin2505@users.noreply.github.com>
2024-03-07 01:42:07 +0000

    Add foreign key constraint for category_id in products table

--- src/schemas/products.schema.sql
+++ src/schemas/products.schema.sql
@@ -6,4 +6,14 @@ CREATE TABLE products (
     price DECIMAL(10, 2),
     stock INT,
     img_url TEXT
+    create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_by VARCHAR(30) -- FOREIGN KEY
 );
+
+-- products Foraneign Keys
+ALTER TABLE products
+ADD CONSTRAINT "fk_category"
+FOREIGN KEY (category_id)
+REFERENCES products_categories(category_id)
+ON UPDATE CASCADE
+ON DELETE CASCADE;
