0a371c5e4ee6846d59513481fbf960a3d186cddf
joshtin2505 <justincastro2505@gmail.com>
2024-03-07 13:21:19 -0500

    .

--- src/schemas/.sql
+++ src/schemas/.sql
@@ -7,14 +7,5 @@ CREATE DATABASE casitadepapel;
 
 -- Create tables
 
-
-
 INSERT INTO users (user_id, name, lastName, email, password, role)
-VALUES (100, 'justin', 'castro p', 'justincastro2505@gmail.com', 'superAdmin', 'masterAdmin');
-
-
--- INSERT INTO users (name, email)
---     VALUES ('joe', 'joe@ibm.com'),
---     ('ryan', 'ryan@faztweb.com');
-
--- select * from users;
+VALUES (1, 'justin', 'castro p', 'justincastro2505@gmail.com', 'superAdmin', 'masterAdmin');
