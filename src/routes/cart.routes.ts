7b6c919b3a937b76a4173a7b43728a9d283be515
joshtin2505 <justincastro2505@gmail.com>
2024-03-07 12:56:04 -0500

    Add cart controller

--- src/routes/cart.routes.ts
+++ src/routes/cart.routes.ts
@@ -1,2 +1,18 @@
 import { Router } from 'express'
-import { cartRoutes } from '../controllers/cart.controllers'
+import {
+  addItemToCart,
+  cartRoutes,
+  deleteItemFromCart,
+  getAllItemsInCart,
+  modifyQuantityProduct
+} from '../controllers/cart.controllers'
+
+const router = Router()
+
+router.get('/', cartRoutes)
+router.get('/all-cart-items', getAllItemsInCart)
+router.post('/add-cart-item', addItemToCart)
+router.put('/update-cart-item', modifyQuantityProduct)
+router.delete('/delete-cart-item/:cartItemId', deleteItemFromCart)
+
+export default router
