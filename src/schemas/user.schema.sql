fb65aa0842de9e98d93ce40febcdb779887a1688
joshtin2505 <justincastro2505@gmail.com>
2024-03-07 14:35:48 -0500

    Refactor same character names

--- src/schemas/user.schema.sql
+++ src/schemas/user.schema.sql
@@ -1,11 +1,11 @@
 CREATE TABLE users (
     user_id SERIAL PRIMARY KEY,
     name VARCHAR(40) NOT NULL,
-    lastName VARCHAR(60),
+    lastn_name VARCHAR(60),
     email TEXT NOT NULL UNIQUE,
     password TEXT NOT NULL,    
     phone INT,
-    phonePrefix INT,
+    phone_prefix INT,
     role VARCHAR(20) DEFAULT 'user' NOT NULL CHECK (role IN ('masterAdmin', 'admin', 'user')),
     create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
