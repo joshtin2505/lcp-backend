57d31c6e0261a2cf2b328ebfedd2bf9e9b8e952c
Justin Castro Perez <91860458+joshtin2505@users.noreply.github.com>
2024-03-23 14:20:32 -0500

    Update cors urls

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,11 @@ import usersRoutes from './routes/users.routes'
 const app = express()
 
 // Middlewares
-app.use(cors())
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Accept']
+  ))
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(cookieParser())
